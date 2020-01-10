0e84bf6ec033207027c83c03336ce7b498b62d82
Calvin Schemanski <calvin@myfab5.com>
2020-01-10 14:35:45 -0500

    make available in production

--- src/DuskServiceProvider.php
+++ src/DuskServiceProvider.php
@@ -15,20 +15,20 @@ class DuskServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Route::get('/_dusk/login/{userId}/{guard?}', [
-            'middleware' => 'web',
-            'uses' => 'Laravel\Dusk\Http\Controllers\UserController@login',
-        ]);
+//         Route::get('/_dusk/login/{userId}/{guard?}', [
+//             'middleware' => 'web',
+//             'uses' => 'Laravel\Dusk\Http\Controllers\UserController@login',
+//         ]);
 
-        Route::get('/_dusk/logout/{guard?}', [
-            'middleware' => 'web',
-            'uses' => 'Laravel\Dusk\Http\Controllers\UserController@logout',
-        ]);
+//         Route::get('/_dusk/logout/{guard?}', [
+//             'middleware' => 'web',
+//             'uses' => 'Laravel\Dusk\Http\Controllers\UserController@logout',
+//         ]);
 
-        Route::get('/_dusk/user/{guard?}', [
-            'middleware' => 'web',
-            'uses' => 'Laravel\Dusk\Http\Controllers\UserController@user',
-        ]);
+//         Route::get('/_dusk/user/{guard?}', [
+//             'middleware' => 'web',
+//             'uses' => 'Laravel\Dusk\Http\Controllers\UserController@user',
+//         ]);
     }
 
     /**
@@ -40,9 +40,9 @@ class DuskServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('production')) {
-            throw new Exception('It is unsafe to run Dusk in production.');
-        }
+//         if ($this->app->environment('production')) {
+//             throw new Exception('It is unsafe to run Dusk in production.');
+//         }
 
         if ($this->app->runningInConsole()) {
             $this->commands([
